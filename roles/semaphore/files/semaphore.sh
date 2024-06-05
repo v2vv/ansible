d45fe6710f879a1653783b0d606d595b5339022c
haitianlan <31479870+v2vv@users.noreply.github.com>
2024-06-06 00:48:00 +0800

    Add files via upload

---
+++ roles/semaphore/files/semaphore.sh
@@ -0,0 +1,4 @@
+wget https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/main/roles/semaphore/files/database.boltdb
+wget https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/fa7eb5ea0cb261fed27cdf23285b531a8901ceca/roles/semaphore/files/config.json
+wget https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/a9cc89ef0ef359e5c3fd04b1937bb180b118a18a/roles/semaphore/files/docker-compose.yaml
+docker compose up -d
