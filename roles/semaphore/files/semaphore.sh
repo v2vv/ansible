054b9ad9ee4d73fc49353cfbdaad6f3fd819f5bb
haitianlan <31479870+v2vv@users.noreply.github.com>
2024-06-06 01:12:41 +0800

    Update semaphore.sh

--- roles/semaphore/files/semaphore.sh
+++ roles/semaphore/files/semaphore.sh
@@ -1,4 +1,4 @@
-wget -N https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/main/roles/semaphore/files/database.boltdb
-wget -N https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/main/roles/semaphore/files/config.json
-wget -N https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/main/roles/semaphore/files/docker-compose.yaml
+wget https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/main/roles/semaphore/files/database.boltdb
+wget https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/main/roles/semaphore/files/config.json
+wget https://hub.gitmirror.com/https://github.com/v2vv/ansible/blob/main/roles/semaphore/files/docker-compose.yaml
 docker compose up -d
