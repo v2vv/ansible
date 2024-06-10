bb7cb0abc9796a6975000162708393c06bf9e5ac
v2vv <v2vvcn@gmail.com>
2024-06-10 13:41:29 +0000

    更新 backup 脚本

--- roles/backup/files/backup.sh
+++ roles/backup/files/backup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 localFilePath=$1
-oneDrivePath=$2
+oneDriveBackupFolder=$2
 client_id=$3
 client_secret=$4
 tenant_id=$5
+
+alist_config_Path='alist/config.json'
+alist_data_path='alist/data.db'
+
+ddnsgo_config_path='ddns-go/.ddnd-go_config.yaml'
+
+semaphore_config_path='semaphore/config.json'
+semaphore_database_path='semaphore/database.boldb'
 
 # echo $localFilePath
 # echo $oneDrivePath
@@ -28,9 +36,45 @@ access_token=$(echo $response | jq -r '.access_token')
 # 输出 access_token
 # echo "Access Token: $access_token"
 
-curl --location --request PUT "https://graph.microsoft.com/v1.0/users/me@lvhongyuan.site/drive/root:/$oneDrivePath:/content" \
---header "Authorization: Bearer $access_token" \
---header 'User-Agent: Apifox/1.0.0 (https://apifox.com)' \
---header 'Content-Type: application/octet-stream' \
---data-binary "@$localFilePath"
+# $1 localFilePath
+# $2 oneDrivePath 
+upload(){
+    curl --location --request PUT "https://graph.microsoft.com/v1.0/users/me@lvhongyuan.site/drive/root:/$1:/content" \
+    --header "Authorization: Bearer $access_token" \
+    --header 'User-Agent: Apifox/1.0.0 (https://apifox.com)' \
+    --header 'Content-Type: application/octet-stream' \
+    --data-binary "@$2"
+}
+
+# 上传 alist
+if [[ -e $localFilePath/$alist_config_Path ]]; then
+    echo "alist Backup File exists."
+    docker stop alist
+    upload $localFilePath/$alist_config_Path $oneDriveBackupFolder/$alist_config_Path
+    upload $localFilePath/$alist_data_path $oneDriveBackupFolder/$alist_data_path
+    docker start alist
+else
+    echo "alist Backup File does not exist."
+fi
+
+
+
+# 上传 ddns-go
+if [[ -e $localFilePath/$ddnsGo_config_path ]]; then
+    echo "ddns-go Backup File exists."
+    upload $localFilePath/$ddnsGo_config_path $oneDriveBackupFolder/$ddnsgo_config_path
+else
+    echo "ddns-go Backup File does not exist."
+fi
+
+
+# 上传 semaphore
+if [[ -e $localFilePath/$semaphore_config_path ]]; then
+    echo "semaphore Backup File exists."
+    upload $localFilePath/$semaphore_config_path $oneDriveBackupFolder/$semaphore_config_path
+    upload $localFilePath/$semaphore_database_path $oneDriveBackupFolder/$semaphore_database_path
+else
+    echo "semaphore Backup File does not exist."
+fi
+
 
